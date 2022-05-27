4a86db33de9fb339ec9b8a023562c756fa10131d
Coderatjsjshors <99674788+husnumustafayevdi@users.noreply.github.com>
2022-05-28 02:23:55 +0400

    Update __init__.py

--- SaitamaRobot/modules/helper_funcs/telethn/__init__.py
+++ SaitamaRobot/modules/helper_funcs/telethn/__init__.py
@@ -1,6 +1,6 @@
-from SaitamaRobot import (DEV_USERS, DRAGONS, DEMONS, TIGERS, WOLVES, telethn)
+from SaitamaRobot import (DRAGONS, DEMONS, TIGERS, WOLVES, telethn)
 
-IMMUNE_USERS = DRAGONS + WOLVES + DEMONS + TIGERS + DEV_USERS
+IMMUNE_USERS = DRAGONS + WOLVES + DEMONS + TIGERS
 
 IMMUNE_USERS = list(DRAGONS) + list(WOLVES) + list(DEMONS) + list(
-    TIGERS) + list(DEV_USERS)
+    TIGERS)
